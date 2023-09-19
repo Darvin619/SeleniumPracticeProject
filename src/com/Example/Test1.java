50572d6b6cf3ab4b74da297ee3363e57187c5b8d
vanga <vanga@LAPTOP-J4T9LKK5>
2023-09-19 20:33:20 +0530

    "First Commit"

---
+++ src/com/Example/Test1.java
@@ -0,0 +1,59 @@
+package com.Example;
+
+public class Test1 {
+	
+	public static String getName() {
+		String name = "James Bond";
+		return name;
+	}
+	public static void main(String[] args) {
+		
+		Test1 Name = new Test1();
+		
+		Name.getName();
+		System.out.println("My name is "+ getName());
+		
+		
+
+//		double temperature = 1;
+//		
+//		if (temperature <= -20) {
+//		System.out.println("Extremly Cold");
+//		
+//		
+//		}else if (-20 < temperature && temperature < 0) {
+//			System.out.println("Cold");
+//			
+//		}else if (0 <temperature && temperature < 15) {
+//			System.out.println("Pleasent");
+//			
+//		} else if (temperature >= 15) {
+//			
+//			System.out.println("Hot");
+//		}
+//		
+//		else {
+//			System.out.println("Invalid Temperature");
+//		
+//		int A=10, B=17, C=17, D=20;
+//		
+//		if ((A==B)||(C==D)){
+//			
+//			System.out.println("One or both Conditions are True");
+//			
+//		}else {
+//			
+//			System.out.println("All the Conditions are false");
+//			
+//			}
+//			
+//		}
+			
+		
+	
+		
+
+	}
+}
+
+
---
+++ src/com/Example/TestLibraries.java
@@ -0,0 +1,26 @@
+package com.Example;
+
+public class TestLibraries {
+
+	public static void main(String[] args) {
+		
+	
+		int a= 10;
+		int b= 20;
+		int sum = a+b;
+		
+		System.out.println(sum);
+	
+
+		if (sum > 8) {
+            System.out.println("Sum is greater than 8.");
+        } else {
+            System.out.println("Sum is less than or equal to 8.");
+		
+		
+			
+		}
+		
+	}
+
+}
